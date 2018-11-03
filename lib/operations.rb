5984a9567bb861db506a157be93793f28a520a06
Blair Carroll <blairacarroll@gmail.com>
2018-11-03 15:39:28 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	else
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	return speed > 60 ? true : false unless speed < 40 == true
+	return speed < 40 ? true : false
 end
-	
-
-
